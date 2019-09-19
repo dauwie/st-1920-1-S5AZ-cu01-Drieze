1d50ade922f58ba8dcdefa2ed55ae046fdc38f75
Dries Deboosere <dries.deboosere@howest.be>
2019-09-19 20:16:28 +0200

    Add new models

---
+++ BookService.WebAPI/Models/Publisher.cs
@@ -0,0 +1,9 @@
+﻿namespace BookService.WebAPI.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
