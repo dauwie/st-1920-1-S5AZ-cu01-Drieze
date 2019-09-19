d7d8d31d3b43c574f39d862139a43079a520fcde
Dries Deboosere <dries.deboosere@howest.be>
2019-09-19 20:44:50 +0200

    Add bookrepository

--- BookService.WebAPI/Startup.cs
+++ BookService.WebAPI/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BookService.WebAPI.Data;
+using BookService.WebAPI.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,8 @@ namespace BookService.WebAPI
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<BookServiceContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BookService")));
+
+            services.AddScoped<BookRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
