4b4c2e5b6a5e02a049903d9cd43fa3260c110f32
Dries Deboosere <dries.deboosere@howest.be>
2019-10-10 18:35:39 +0200

    Fix selfreferencing loop

--- BookService.WebAPI/Startup.cs
+++ BookService.WebAPI/Startup.cs
@@ -28,7 +28,9 @@ namespace BookService.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
             services.AddDbContext<BookServiceContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BookService")));
 
             services.AddScoped<BookRepository>();
